3d5db08b51c702f2a4b9fb3475f6e72bc5d4c986
AlistairKrieck <144819210+AlistairKrieck@users.noreply.github.com>
2025-05-13 14:28:20 -0400

    Update MobBlock.cs

--- BrickBreaker/MobBlock.cs
+++ BrickBreaker/MobBlock.cs
@@ -1,1 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace BrickBreaker
+{
+    public class MobBlock : Block
+    {
+        public string mobType;
+        public int xSpeed = 5;
+        public int ySpeed = 5;
+        public Color mobColor;
+
+        public static string[] mobTypes = { "skeleton", "zombie", "creeper" };
+
+        public MobBlock(int _x, int _y) : base(_x, _y)
+        {
+
+        }
+
+        public void Move(string direction)
+        {
+            if (direction == "left")
+            {
+                x -= xSpeed;
+            }
+            if (direction == "right")
+            {
+                x += xSpeed;
+            }
+
+            if (direction == "up")
+            {
+                y -= ySpeed;
+            }
+            if (direction == "down")
+            {
+                y += ySpeed;
+            }
+
+        }
+    }
+}
 ﻿
