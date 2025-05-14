9a3f39639510aa922570576d429410ddffec66bf
Silas Campbell <Silas Campbell@WIN-H6R3K5TUMB1>
2025-05-14 13:41:38 -0400

    Newest version with blocks and mobblocks

--- BrickBreaker/Block.cs
+++ BrickBreaker/Block.cs
@@ -9,6 +9,21 @@ namespace BrickBreaker
 {
     public class Block
     {
-        
+        public int x, y;
+        public static int width = 40;
+        public static int height = 40;
+        public static int numRows = 5;
+        public static int numCols = 10;
+        public static int spacing = 5;
+        public Rectangle Rect { get; set; }
+        public Brush Color { get; set; }
+
+        public Block(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+
+            Rect = new Rectangle(x, y, width, height);
+        }
     }
 }
